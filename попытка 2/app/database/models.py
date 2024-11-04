76ce6195d8d74d01d14155860336af3c8eec2308
misarrw <sk074866336@gmail.com>
2024-11-04 19:57:38 +0300

    что-то с ошибками в бд

--- попытка 2/app/database/models.py
+++ попытка 2/app/database/models.py
@@ -1,4 +1,30 @@
+from sqlalchemy import BigInteger, String, ForeignKey
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from sqlalchemy.ext.asyncio import AsyncAttrs, async_sessionmaker, create_async_engine
 
-engine = create_async_engine(url='')
+
+engine = create_async_engine(url = 'mysql+aiomysql://root:2oGuJ2izY8RwIe1@localhost:3306/bot')
+
+async_session = async_sessionmaker(engine)
+
+class Base(AsyncAttrs, DeclarativeBase): 
+    pass
+
+class User(Base):
+    __tablename__ = 'users'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    tg_id = mapped_column(BigInteger)
+    number_gr: Mapped[str] = mapped_column(String(10))
+    status: Mapped[bool] = mapped_column()
+
+class Password(Base):
+    __tablename__ = 'passwords'
+    id: Mapped[int] = mapped_column(primary_key = True)
+    number_gr: Mapped[str] = mapped_column(String(6))
+    password: Mapped[str] = mapped_column(String(50))
+
+    '''category: Mapped[int] = mapped_column(ForeignKey('categories.id'))'''
+
+async def async_main():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
