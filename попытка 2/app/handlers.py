fc01e5cc2e0f3a88e3d77a13a55bb7cf012aabf5
misarrw <sk074866336@gmail.com>
2024-11-06 00:27:11 +0300

    добавила регистрацию

--- попытка 2/app/handlers.py
+++ попытка 2/app/handlers.py
@@ -3,19 +3,79 @@ from aiogram.types import Message, CallbackQuery
 from aiogram.filters import CommandStart, Command
 from aiogram.fsm.state import State, StatesGroup
 from aiogram.fsm.context import FSMContext
+import app.database.requests as rq
 
 import app.keyboards as kb
 
 class Register(StatesGroup):
-    name = State()
-    surname = State() # посмотри, что такое классы
+    group = State()
+    status = State() # посмотри, что такое классы
+    password = State()
 
 router = Router()
+c = 0
 
 @router.message(CommandStart())
-async def cmd_start(message: Message):
-    await message.answer('иу, тебе тоже про меня рассказали...\nменя зовут... а, впрочем, неважно.\nчто надо? не задерживай меня',
-                         reply_markup=kb.main)
+async def cmd_start(message: Message, state: FSMContext):
+    '''await message.answer('иу, тебе тоже про меня рассказали...\nменя зовут... а, впрочем, неважно.\nчто надо?.', reply_markup=kb.parents)'''
+    '''if await rq.set_user_id(message.from_user.id) == False:'''
+    '''    await message.answer('продолжим с регистрацией?', reply_markup=kb.choice)'''
+    '''else:'''
+    '''    await message.answer('ты можешь получить доступ к личной инфе')'''
+    await message.answer('иу, тебе тоже про меня рассказали...\nменя зовут... а, впрочем, неважно.')
+    if await rq.set_user_id(message.from_user.id) == False:
+        await message.answer('мне нужно узнать, кто ты. зарегистрируйся, для начала')
+        await state.set_state(Register.group)
+        await message.answer('выбери свою бибику (группу)',
+                         reply_markup=kb.reg_groups)
+    else:
+        await message.answer('что надо?')
+
+@router.message(Register.group)
+async def register_group(message: Message, state: FSMContext):
+    await state.update_data(group = message.text)
+    await state.set_state(Register.password)
+    await message.answer('окей. введи пароль, если ты (зам)староста', reply_markup=kb.skip)
+
+@router.message(Register.password)
+async def check_password(message: Message, state: FSMContext):
+    global c
+    data_interim_group = await state.get_data()
+    if message.text == 'нет, скип':
+        await state.update_data(password = message.text)
+        await state.set_state(Register.status)
+    else:
+        check = await rq.check_password(data_interim_group['group'], message.text)
+        if check:
+           await state.update_data(password=message.text)
+           await state.set_state(Register.status)
+           await message.answer('верю')
+        elif not check and c<3:
+           c+=1
+           await state.update_data(password=message.text)
+           await state.set_state(Register.password)
+           await message.answer(f'неправильно. еще {3-c} попытки')
+        elif c==3:
+            await state.set_state(Register.status)
+            await message.answer('не верю')
+            await message.answer('нажми, чтобы продолжить',
+                        reply_markup=kb.continue_)
+
+@router.message(Register.status)
+async def reg_st(message: Message, state: FSMContext):
+    data_interim_group=await state.get_data()
+    check=await rq.check_password(data_interim_group['group'],data_interim_group['password'])
+    if not(check):
+        await state.update_data(status=False)
+    else:
+        await state.update_data(status=True)
+    data_reg=await state.get_data()
+    await rq.set_user(data_reg['group'], message.from_user.id, data_reg['status'])
+    await message.answer('поехалииииии')
+
+@router.callback_query(F.data == 'no')
+async def yes(callback: CallbackQuery):
+    await callback.message.answer('ну ок', reply_markup=kb.main)
 
 
 @router.message(Command('help'))
@@ -73,46 +133,46 @@ async def contact6(callback: CallbackQuery):
 
 
 
-@router.callback_query(F.data == '241')
-async def group241(callback: CallbackQuery):
-    await callback.message.answer('б241иб')
+'''@router.callback_query(F.data == '241')'''
+'''async def group241(callback: CallbackQuery):'''
+'''    await callback.message.answer('б241иб')'''
 
-@router.callback_query(F.data == '242')
-async def group241(callback: CallbackQuery):
-    await callback.message.answer('б242иб')
+'''@router.callback_query(F.data == '242')'''
+'''async def group241(callback: CallbackQuery):'''
+'''    await callback.message.answer('б242иб')'''
 
-@router.callback_query(F.data == '243')
-async def group241(callback: CallbackQuery):
-    await callback.message.answer('б243иб')
+'''@router.callback_query(F.data == '243')'''
+'''async def group241(callback: CallbackQuery):'''
+'''    await callback.message.answer('б243иб')'''
 
-@router.callback_query(F.data == '244')
-async def group241(callback: CallbackQuery):
-    await callback.message.answer('б244иб')
+'''@router.callback_query(F.data == '244')'''
+'''async def group241(callback: CallbackQuery):'''
+'''    await callback.message.answer('б244иб')'''
 
-@router.callback_query(F.data == '245')
-async def group241(callback: CallbackQuery):
-    await callback.message.answer('б245иб')
+'''@router.callback_query(F.data == '245')'''
+'''async def group241(callback: CallbackQuery):'''
+'''    await callback.message.answer('б245иб')'''
 
 
 
-@router.message(Command('register'))
-async def register(message: Message, state: FSMContext):
-    await state.set_state(Register.name)
-    await message.answer('кто ты есть?')
+'''@router.message(Command('register'))'''
+'''async def register(message: Message, state: FSMContext):'''
+'''    await state.set_state(Register.name)'''
+'''    await message.answer('кто ты есть?')'''
 
-@router.message(Register.name)
-async def register_name(message: Message, state: FSMContext):
-    await state.update_data(name=message.text)
-    await state.set_state(Register.surname)
-    await message.answer('а по фамилии?')
+'''@router.message(Register.name)'''
+'''async def register_name(message: Message, state: FSMContext):'''
+'''    await state.update_data(name=message.text)'''
+'''    await state.set_state(Register.surname)'''
+'''    await message.answer('а по фамилии?')'''
 
-@router.message(Register.surname)
-async def register_surname(message: Message, state: FSMContext):
-    await state.update_data(surname=message.text)
-    data = await state.get_data()
-    await message.answer(f'перепроверь машину\nимя: {data["name"]}\nфамилия: {data["surname"]}')
+'''@router.message(Register.surname)'''
+'''async def register_surname(message: Message, state: FSMContext):'''
+'''    await state.update_data(surname=message.text)'''
+'''    data = await state.get_data()'''
+'''    await message.answer(f'перепроверь машину\nимя: {data["name"]}\nфамилия: {data["surname"]}')'''
 ### команда вверху вынимает значения
-    await state.clear()
+'''    await state.clear()'''
 ### команда сверху чистит записанные данные, чтобы бот не засорялся
 
 
--- попытка 2/main.py
+++ попытка 2/main.py
@@ -1,13 +1,16 @@
+import os
 import asyncio
 from aiogram import Bot, Dispatcher
+from dotenv import load_dotenv
 
 from app.handlers import router
 
-bot = Bot(token='7946627017:AAEZpHwYuXV5syytjvclO2OQ_49S7zVRp7c')
-dp = Dispatcher() # занимается обработкой сообщений от пользователя
-
+from app.database.models import async_main
 
 async def main():
+    await async_main()
+    bot = Bot(token='7946627017:AAEZpHwYuXV5syytjvclO2OQ_49S7zVRp7c')
+    dp = Dispatcher() # занимается обработкой сообщений от пользователя
     dp.include_router(router)
     await dp.start_polling(bot)
 
